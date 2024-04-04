81e880766b8e5dcfe0a12fcd0ba3121f3b9a93bf
Gabriel Joaquim <gabriel.joaquim.5567@gmail.com>
2024-04-04 12:36:48 -0300

    feat: adicionado layout básico e funcionalidade do slider
    Adicionei os botões e labels e a funcionalidade do slider

---
+++ TipTime/TipTime/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnBtn15_Clicked(object sender, EventArgs e)
+        {
+            TipPercentOut.Text = "15%";
+            TipSlider.Value = 15;
+        }
+
+        private void OnBtn30_Clicked(object sender, EventArgs e)
+        {
+            TipPercentOut.Text = "30%";
+            TipSlider.Value = 30;
+        }
+
+        private void OnRoundUp_Clicked(object sender, EventArgs e)
+        {
+            double valorRefeicao = Convert.ToDouble(ValueEntry.Text);
+            double valorSlider = Convert.ToDouble(TipSlider.Value);
+            double valorGorjeta = valorRefeicao * (valorSlider / 100);
+        }
+
+        private void OnRoundDn_Clicked(object sender, EventArgs e)
+        {
+            double valorRefeicao = Convert.ToDouble(ValueEntry.Text);
+            double valorSlider = Convert.ToDouble(TipSlider.Value);
+            double valorGorjeta = valorRefeicao * (valorSlider / 100);
+        }
+
+        private void OnTipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double sliderValue = TipSlider.Value;
+            string sliderValueOut = sliderValue.ToString() + '%';
+            TipPercentOut.Text = sliderValueOut.ToString();
+        }
+    }
+
+}
